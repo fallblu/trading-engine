08dd8d47b97991a58566eccfc71e6f6b0ceee836
James Mallette <jamesmallmw@gmail.com>
2026-08-22 11:16:33 -0400

    fix: preserve legacy strategy compatibility

--- lib/contract.ml
+++ lib/contract.ml
@@ -1,12 +1,13 @@
 let version = "6"
 let previous_version = "5"
-let legacy_journal_version = "4"
+let legacy_journal_version = "3"
 
 let supported_versions =
-  [ version; previous_version; legacy_journal_version; "3" ]
+  [ version; previous_version; "4"; legacy_journal_version ]
 
 let is_supported version = List.mem version supported_versions
 let strategy_protocol_version = "4"
+let previous_strategy_protocol_version = "3"
 let engine_version = "1.0.0"
 let strings values = `List (List.map (fun value -> `String value) values)
 
@@ -20,7 +21,9 @@ let capabilities_to_yojson () =
       ("journal_formats", strings [ "jsonl" ]);
       ("execution_models", strings Execution_model.supported);
       ("execution_model_contracts", Execution_model.capabilities_to_yojson ());
-      ("strategy_protocol_versions", strings [ strategy_protocol_version ]);
+      ( "strategy_protocol_versions",
+        strings
+          [ strategy_protocol_version; previous_strategy_protocol_version ] );
       ("resource_limits", Resource_limits.to_yojson ());
     ]
 
